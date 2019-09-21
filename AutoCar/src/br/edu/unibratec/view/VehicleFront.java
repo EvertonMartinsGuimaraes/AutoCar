f2e92f34656d989a2c4b30a1c37a795f08eccdc0
Flavio Bastos <flavio.bastos@hotmail.com>
2019-09-21 15:31:42 -0300

    testando github

--- AutoCar/src/br/edu/unibratec/view/VehicleFront.java
+++ AutoCar/src/br/edu/unibratec/view/VehicleFront.java
@@ -1,12 +1,13 @@
 package br.edu.unibratec.view;
+import br.edu.unibratec.model.Car;
 
 public class VehicleFront {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		System.out.println();
+		Car getWaterLevel = new Car();
+		System.out.println(getWaterLevel.getCurrentKm());
 		
 	}
 
