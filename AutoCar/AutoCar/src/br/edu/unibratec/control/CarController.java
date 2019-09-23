4e37677953337dc5f89a310472a10a4532e67b2f
Victor Lins <victorliins93@gmail.com>
2019-09-23 15:33:59 -0300

    Alteração no CarController

--- AutoCar/AutoCar/src/br/edu/unibratec/control/CarController.java
+++ AutoCar/AutoCar/src/br/edu/unibratec/control/CarController.java
@@ -1,17 +1,21 @@
 package br.edu.unibratec.control;
 
+import br.edu.unibratec.model.CarFiat;
 import br.edu.unibratec.model.CarVolkswagem;
 import interfaces.ICarModel;
 
 public class CarController {
 	private ICarModel car;
 
-//ALTERAR VEICULO NESSE METODO.
+	/*ALTERAR VEICULO NESSE METODO.
+	METODO CONSTRUTOR DO VEICULO.
+	*/
 	public CarController() {
-		car = new CarVolkswagem();
+		car = new CarFiat();
 
 	}
 
+//METODO GET DE ACESSO AO OBJETO CARMODEL.
 	public ICarModel getCarModel() {
 		return this.car;
 	}
