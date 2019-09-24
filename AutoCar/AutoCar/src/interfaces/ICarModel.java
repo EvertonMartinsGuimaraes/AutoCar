9b51860f98d044e4fbd56d2e1dfb83ca0b2f1ee1
Victor Lins <victorliins93@gmail.com>
2019-09-24 12:00:57 -0300

    alteracao ICarModel

--- AutoCar/AutoCar/src/interfaces/ICarModel.java
+++ AutoCar/AutoCar/src/interfaces/ICarModel.java
@@ -3,31 +3,57 @@ package interfaces;
 //INTERFACE MODELO DOS CARROS A SEREM CRIADOS.
 public interface ICarModel {
 
-	public void setOilReduction(int kmDistance, double routeRate);
+	// METODOS DE CALCULO PARA SETAR
+	public void oilReduction(int kmDistance, double routeRate);
 
-	public void setWaterReduction(int kmDistance, double routeRate);
+	public void waterReduction(int kmDistance, double routeRate);
 
-	public void setGasReduction(int kmDistance, double routeRate);
+	public void gasReduction(int kmDistance, double routeRate);
 
-	public void setKmReduction(int kmDistance, double routeRate);
+	public void newKm(int kmDistance, double routeRate);
+
+	public void kmForReview();
+
+	public void newReview();
 	
-	public void overhaul(int refillGas,int refillOil, int refillWatter);
-	
+	// METODOS GET DOS VALORES CALCULADOS
+	public int getKm();
+
+	public double getGas();
+
+	public double getWater();
+
+	public double getOil();
+
+	public int getReview();
+
+	public int getRemaining();
+
+	// METODOS SET E GET DO CARRO JA SETADO
+	public void setOilReduction();
+
+	public void setWaterReduction();
+
+	public void setGasReduction();
+
+	public void setNewKm();
+
 	public void setKmForReview();
 
 	public void setNewReview();
-	
+
+	public void overhaul(int refillGas, int refillOil, int refillWatter);
+
 	public double getOilLevel();
-	
+
 	public double getGasLevel();
-	
+
 	public double getWaterLevel();
-	
+
 	public int getCurrentKm();
-	
+
 	public int getRemainingKm();
-	
+
 	public int getNextReview();
-	
-		
-	}
+
+}
