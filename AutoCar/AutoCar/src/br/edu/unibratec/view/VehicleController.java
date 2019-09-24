f2e6663067f65f1520b5461a19ea9976b88f53a4
Victor Lins <victorliins93@gmail.com>
2019-09-24 12:04:12 -0300

    Alteracao VehicleController

--- AutoCar/AutoCar/src/br/edu/unibratec/view/VehicleController.java
+++ AutoCar/AutoCar/src/br/edu/unibratec/view/VehicleController.java
@@ -4,7 +4,6 @@ import java.text.DecimalFormat;
 
 import br.edu.unibratec.control.CarController;
 import br.edu.unibratec.control.PlaceController;
-import br.edu.unibratec.model.Place;
 import br.edu.unibratec.model.Place.ROUTE_TYPE;
 
 public class VehicleController {
@@ -17,22 +16,23 @@ public class VehicleController {
 
 		DecimalFormat df = new DecimalFormat("###,##0");
 
-		System.out.println("\nGasolina:" + df.format(this.carC.getCarModel().getGasLevel()) + " %\nKm atual: "
+		System.out.println("\nGasolina:" + df.format(this.carC.getCarModel().getGasLevel()) + " %\nQuilometragem: "
 				+ this.carC.getCarModel().getCurrentKm() + " KM\nProxima revisao: "
 				+ this.carC.getCarModel().getNextReview() + " KM\nOleo:"
 				+ df.format(this.carC.getCarModel().getOilLevel()) + " ML\nKm para proxima revisao: "
 				+ this.carC.getCarModel().getRemainingKm() + " KM\nNivel da agua: "
 				+ df.format(this.carC.getCarModel().getWaterLevel()) + " ML\n\n");
 	}
+	
 
 	//MOSTRA O STATUS DA VIAGEM FEITA
 	public void tripStatus(String nome, int distance) {
 
-		System.out.println("\n\nViagem feita para: " + nome + " / Distancia percorrida: " + distance);
+		System.out.println("\n\nViagem feita para: " + nome + " / Distancia percorrida: " + distance+"KM");
 	}
 	
-	// SETA AS REDUÇOES APOS CONFIRMAÇÃO DA ROTA
-	public void setTrack(int km, ROUTE_TYPE type) {
+	//FAZ OS CALCULOS DE UMA POSSIVEL ROTA
+	public void calcTrack(int km, ROUTE_TYPE type) {
 		double routeRate = 0;
 
 		if (type == ROUTE_TYPE.URBAN) {
@@ -41,12 +41,32 @@ public class VehicleController {
 		} else if (type == ROUTE_TYPE.ROADWAY) {
 			routeRate = 0.98;
 		}
-		this.carC.getCarModel().setGasReduction(km, routeRate);
+		this.carC.getCarModel().gasReduction(km, routeRate);
+		this.carC.getCarModel().kmForReview();
+		this.carC.getCarModel().newKm(km, routeRate);
+		this.carC.getCarModel().newReview();
+		this.carC.getCarModel().oilReduction(km, routeRate);
+		this.carC.getCarModel().waterReduction(km, routeRate);
+		
+		DecimalFormat df = new DecimalFormat("###,##0");
+
+		System.out.println("\nGastos Previstos:\n\n\nGasolina:" + df.format(this.carC.getCarModel().getGas()) + " %\nQuilometragem: "
+				+ this.carC.getCarModel().getKm() + " KM\nProxima revisao: "
+				+ this.carC.getCarModel().getReview() + " KM\nOleo:"
+				+ df.format(this.carC.getCarModel().getOil()) + " ML\nKm para proxima revisao: "
+				+ this.carC.getCarModel().getRemaining() + " KM\nNivel da agua: "
+				+ df.format(this.carC.getCarModel().getWater()) + " ML\n\n");
+	}
+
+	// SETA AS REDUÇOES APOS CONFIRMAÇÃO DA ROTA
+	public void setTrack() {
+
+		this.carC.getCarModel().setNewKm();
+		this.carC.getCarModel().setGasReduction();
 		this.carC.getCarModel().setKmForReview();
-		this.carC.getCarModel().setKmReduction(km, routeRate);
 		this.carC.getCarModel().setNewReview();
-		this.carC.getCarModel().setOilReduction(km, routeRate);
-		this.carC.getCarModel().setWaterReduction(km, routeRate);
+		this.carC.getCarModel().setOilReduction();
+		this.carC.getCarModel().setWaterReduction();
 	}
 
 }
